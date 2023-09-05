c9f2e939655ff305db9edb00958a0d5cceabba71
cardnl <100021830+cardnl@users.noreply.github.com>
2023-09-05 16:15:17 +1000

    Update leaflet.drillbounds.js

--- leaflet.drillbounds.js
+++ leaflet.drillbounds.js
@@ -1,163 +1,137 @@
-L.DrillBoundaries = L.LayerGroup.extend({
-    initialize: function (options) {
-        const { styles } = options || { styles: {} };
-        L.setOptions(this, options);
-        
-        this._listeners = { click: () => {} };
-        this._levelAt = 0;
-        this._levels = [];
-        this._enabled = true;
+L.DrillBoundaries = L.FeatureGroup.extend({
+  initialize: function (options) {
+    L.Util.setOptions(this, options);
+    this._levels = [{ group: L.featureGroup(), selected: null }];
+    this._levels[0].group.addEventParent(this);
+    this._currLevel = 0;
+    this._enabled = true;
 
-        // Change to CSS
-        this._styles = {
-            hidden: {
-		fill: false,
-		color: "transparent",
-		weight: 0,
-            },
-            default: {
-		fill: true,
-		color: "white",
-		fillColor: "#777",
-		fillOpacity: 0.5,
-		weight: 2,
-                ...(styles.default || {})
-            },
-            selected:{
-		"fill": false,
-		"color": "#ffaf3b",
-		"weight": 5,
-                ...(styles.selected || {})
-	    },
-            selected2:{
-		"fill": true,
-		fillColor: "#777",
-		fillOpacity: 0.5,
-		"color": "#ffaf3b",
-		"weight": 5,
-                ...(styles.selected2 || {})
-	    }
-        };
-        
-    },
-    _headLevel: function () {
-        return this._levels[this._levels.length - 1];
-    },
-    _showLevel: function (level) {
-        let levelShown;
-        for (let i = 0; i < this._levels.length; i++) {
-            for (let layer of this._levels[i].layers) {
-                // Hide all layers, unless i === level
-                if (i === level) {
-                    levelShown = this._levels[i];
-                    layer.setStyle(this._styles.default);
-                } else {
-                    layer.setStyle(this._styles.hidden);
-                }
-            }       
-        }
-        return levelShown;
-    },
-    setLevel: function (level) {
-        const isLastLevel = level === this._levels.length;
-        const prevLevel = this._levels[level - 1] || false;
-        
-        if (level >= 0 && level < this._levels.length) {
-            // Show & set the level
-            const nextLevel = this._showLevel(level);
-            nextLevel.layers.forEach(l => l.bringToBack());
-            this._levelAt = level;
-        } else if (isLastLevel) {
-            // If last level, hide all layers
-            this._showLevel(-1);
-            this._levelAt = level;
-        }
-        
-        // If previous layer exists, show it's selection
-        if (prevLevel && prevLevel.selected) {
-            this._selectLayer(prevLevel.selected);
-            return prevLevel.selected;
-        } else {
-            return null;
-        }
-    },
-    addLevel: function (layer) {
-        this._levels.push({ layers: [], selected: null });
-    },
-    _setSelected: function (layer) {
-        if (this._levelAt < this._levels.length) {
-            this._levels[this._levelAt].selected = layer;
-        }
-    },
-    _pointInSelected: function (latLng) {
-        if (this._levelAt > 0) {
-            const lastSelect = this._levels[this._levelAt - 1].selected;
-            const lastAsGeoJSON = L.geoJSON(lastSelect.feature);
-            return leafletPip.pointInLayer(latLng, lastAsGeoJSON, true).length;
-        }
-        return true;
-    },
-    _layerIsSelected: function (layer) {
-        if (this._levelAt > 0) {
-            const lastSelect = this._levels[this._levelAt - 1].selected;
-            return layer === lastSelect;
-        }
-        return false;
-    },
-    drillUp: function() {
-        return this.setLevel(this._levelAt - 1);
-    },
-    drillDown: function() {
-        return this.setLevel(this._levelAt + 1);
-    },
-    _selectLayer: function(layer) {
-        layer.setStyle(this._styles.selected);
-    },
-    addLayer: function (layer) {
-        if (layer instanceof L.LayerGroup) {
-            this._headLevel().layers.push(layer);
-        }
-    },
-    addGeoJSON: function (geojson) {
-        const layer = L.geoJSON(geojson, {
-	    style: this._styles.default,
-	    onEachFeature: (feature, currLayer) => (
-		// Send feature information to click
-		currLayer.on('click', (evt) => {
-                    // Check if click is in bounds
-                    if (!this._pointInSelected(evt.latlng) ||
-                        this._layerIsSelected(currLayer) || !this._enabled) {
-                        return;
-                    }
-                    
-                    // Select this layer
-                    this._setSelected(currLayer);
-                    
-                    // Go to the next level level
-                    this.drillDown();
-
-                    this._listeners.click(evt, currLayer);
-		})
-	    )
-        });
-        this.addLayer(layer);
-        return layer;
-    },
-    addToMap: function(map) {
-        // Add all layers to the map
-        for (let level of this._levels) {
-            for (let layer of level.layers) {
-                map.addLayer(layer);
-            }
-        }
-    },
-    on: function (type, callback) {
-        this._listeners[type] = callback;
-    },
-    toggle: function (val) {
-        this._enabled = val;
+    this._styles = {
+      hidden: {
+        fill: false,
+        weight: 0
+      },
+      default: {
+        fill: true,
+        fillColor: '#777',
+        fillOpacity: 0.5,
+        color: 'white',
+        weight: 2
+      },
+      selected: {
+        fill: false,
+        color: '#ffaf3b',
+        weight: 5
+      },
+      ...({} || options.styles)
+    };
+  },
+  _getSelected: function () {
+    if (this._currLevel > 0) {
+      return this._levels[this._currLevel - 1].selected;
     }
+    return null;
+  },
+  _pointInSelected: function (latLng) {
+    if (this._currLevel > 0) {
+      const lastSelect = this._getSelected();
+      const lastAsGeoJSON = L.geoJSON(lastSelect.feature);
+      return leafletPip.pointInLayer(latLng, lastAsGeoJSON, true).length;
+    }
+    return true;
+  },
+  _showLevel: function (level) {
+    this._levels.forEach(i => i.group.setStyle(this._styles.hidden));
+    if (level >= 0 && level < this._levels.length) {
+      this._currLevel = level;
+      this._levels[level].group.setStyle(this._styles.default);
+    }
+    return this._levels[this._currLevel];
+  },
+  _drillDown: function (level, layer) {
+    this._levels[this._currLevel].selected = layer;
+    const nextLevel = this._showLevel(this._currLevel + 1);
+
+    // Bring next level to back, so selected is on top
+    nextLevel.group.getLayers().forEach(l => l.bringToBack());
+    layer.setStyle(this._styles.selected);
+
+    return layer;
+  },
+  drillUp: function () {
+    if (this._currLevel <= 0) {
+      return;
+    }
+
+    const currSelected = this._levels[this._currLevel].selected;
+
+    // If current level has selected, unselect it first
+    const nextLevel = this._showLevel(
+      this._currLevel - currSelected ? 0 : 1
+    );
+    this._levels[this._currLevel].selected = null;
+
+    // If previous level exists
+    if (this._currLevel > 0) {
+      // Bring back & select next level
+      nextLevel.group.getLayers().forEach(l => l.bringToBack());
+      const nextSelected = this._levels[this._currLevel - 1].selected;
+      nextSelected.setStyle(this._styles.selected);
+      if (this._map) {
+        this._map.flyToBounds(nextSelected);
+      }
+      this.fire('drill', { layer: nextSelected });
+    }
+  },
+  addLayerAt: function (level, layer) {
+    while (this._levels.length <= level) {
+      this._levels.push({ group: L.featureGroup(), selected: null });
+    }
+    this._levels[level].group.addLayer(layer);
+    this.fire('layeradd', { layer: layer });
+
+    // Show current level
+    layer.setStyle({ className: 'drillbounds-layer' });
+    if (level === this._currLevel) {
+      layer.setStyle(this._styles.default);
+    } else {
+      layer.setStyle(this._styles.hidden);
+    }
+
+    // Bind click to each feature
+    layer.eachLayer(layer => {
+      layer.on('click', evt => {
+        if (!this._pointInSelected(evt.latlng) ||
+          this._getSelected() === layer || !this._enabled) {
+          return;
+        }
+        this._drillDown(level, layer);
+        this.fire('drill', { layer: layer });
+        this.fire('click', { layer: layer });
+        if (this._map) {
+          evt.target._map.flyToBounds(layer);
+        }
+      });
+    });
+
+    return this;
+  },
+  addLayer: function (layer) {
+    return this.addLayerAt(this._currLevel, layer);
+  },
+  onAdd: function (map) {
+    this._map = map;
+    this._levels.forEach(level => level.group.addTo(map));
+  },
+  onRemove: function (map) {
+    this._map = null;
+    this._levels.forEach(level => level.group.remove());
+  },
+  toggle: function (value) {
+    this._enabled = value;
+  }
 });
-L.drillBoundaries = function(options) {
-    return new L.DrillBoundaries(options);
+
+L.drillBoundaries = function (options) {
+  return new L.DrillBoundaries(options);
 };
